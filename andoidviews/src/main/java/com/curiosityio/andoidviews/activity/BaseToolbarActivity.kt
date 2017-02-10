6a3b1147591f5d37cefc897151b1fa394adb7796
Levi Bostian <levi@curiosityio.com>
2017-02-10 10:59:13 -0600

    Give ability for children of BaseToolbarActivity to have fragment behind toolbar instead of below it.

--- andoidviews/src/main/java/com/curiosityio/andoidviews/activity/BaseToolbarActivity.kt
+++ andoidviews/src/main/java/com/curiosityio/andoidviews/activity/BaseToolbarActivity.kt
@@ -3,9 +3,12 @@ package com.curiosityio.andoidviews.activity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.RelativeLayout
 import com.curiosityio.andoidviews.R
+import kotlinx.android.synthetic.main.activity_base_toolbar.*
 
-abstract class BaseToolbarActivity() : BaseActivity() {
+abstract class BaseToolbarActivity : BaseActivity() {
 
     protected lateinit var toolbar: Toolbar
 
@@ -24,6 +27,9 @@ abstract class BaseToolbarActivity() : BaseActivity() {
     // Override to provide your own Toolbar.
     open fun getToolbarId(): Int = R.id.toolbar
 
+    // Override to decide if you want the embedded fragment under the toolbar or below it. Good for when you want to use a transparent toolbar.
+    open fun setFragmentBelowToolbar(): Boolean = true
+
     // Override to provide own color of Toolbar.
     open fun getToolbarBackground(): Int = R.attr.colorPrimary
 
@@ -32,6 +38,11 @@ abstract class BaseToolbarActivity() : BaseActivity() {
         toolbar.setBackgroundColor(getToolbarBackground())
 
         setSupportActionBar(toolbar)
+
+        if (setFragmentBelowToolbar()) {
+            val params = activity_base_toolbar_fragment_container.layoutParams as RelativeLayout.LayoutParams
+            params.addRule(RelativeLayout.BELOW, getToolbarId())
+        }
     }
 
     override fun setContentView(layoutResID: Int) {
